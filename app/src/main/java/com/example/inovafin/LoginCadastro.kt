5f3e735acafdd8d3518f6479d730692a21dfeac3
Erick Barauna <ericksbarauna26@gmail.com>
2023-09-21 08:14:54 -0300

    Binding configurado

--- app/src/main/java/com/example/inovafin/LoginCadastro.kt
+++ app/src/main/java/com/example/inovafin/LoginCadastro.kt
@@ -2,10 +2,15 @@ package com.example.inovafin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.inovafin.databinding.ActivityLoginCadastroBinding
 
 class LoginCadastro : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginCadastroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_cadastro)
+        binding = ActivityLoginCadastroBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
