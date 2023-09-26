035b50e296d1d6eb4698193dea3fc8fcdea48863
Erick Barauna <ericksbarauna26@gmail.com>
2023-09-26 08:05:50 -0300

    Logica de cadastro sem tratamento

--- app/src/main/java/com/example/inovafin/Cadastro.kt
+++ app/src/main/java/com/example/inovafin/Cadastro.kt
@@ -1,11 +1,24 @@
 package com.example.inovafin
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.inovafin.databinding.ActivityCadastroBinding
+import com.google.gson.JsonObject
+import com.koushikdutta.async.future.FutureCallback
+import com.koushikdutta.ion.Ion
 
 class Cadastro : AppCompatActivity() {
+
+    var Host = "https://inovafin.000webhostapp.com/projeto/inserir.php"
+    var url: String? = null
+    var ret: String? = null
+
+    var nome: String? = null
+    var email: String? = null
+    var senha: String? = null
 
     private lateinit var binding: ActivityCadastroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +27,38 @@ class Cadastro : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        fun inserir() {
+            nome = binding.nomeUsuario.text.toString()
+            email = binding.emailUsuario.text.toString()
+            senha = binding.senhaUsuario.text.toString()
+
+            url = Host
+
+            try {
+                Ion.with(this)
+                    .load(url)
+                    .setBodyParameter("nome", nome)
+                    .setBodyParameter("email", email)
+                    .setBodyParameter("senha", senha)
+                    .asJsonObject()
+                    .setCallback(FutureCallback<JsonObject> { e, result ->
+                        if (e != null) {
+                            // Ocorreu um erro na solicitação HTTP
+                            Toast.makeText(applicationContext, "Erro na solicitação: " + e.localizedMessage, Toast.LENGTH_LONG).show()
+                        } else {
+                            ret = result["status"].asString
+                            if (ret == "ok")
+                                Toast.makeText(applicationContext, "Incluído com sucesso", Toast.LENGTH_LONG).show()
+                            else
+                                Toast.makeText(applicationContext, "Erro", Toast.LENGTH_LONG).show()
+                        }
+                    })
+            } catch (e: Exception) {
+                // Lidar com exceções gerais aqui
+                Toast.makeText(applicationContext, "Erro geral: " + e.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+        }
+
         // Navegação para Tela LoginCadastro
         binding.icVoltar.setOnClickListener {
             var voltarTela = Intent(this, LoginCadastro::class.java)
@@ -21,9 +66,9 @@ class Cadastro : AppCompatActivity() {
         }
 
         // Navegação para Tela Home
-//        binding.btCadastro.setOnClickListener {
-//            var navegarTelaHome = Intent(this, Home::class.java)
-//            startActivity(navegarTelaHome)
-//        }
+        binding.btCadastro.setOnClickListener {
+            inserir()
+        }
+
     }
 }
